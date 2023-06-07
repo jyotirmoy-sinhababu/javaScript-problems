b8159edc72bd73f6ed4b5e54c47127e44f026ade
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-07 16:20:42 +0530

    another question added

--- app.js
+++ app.js
@@ -69,23 +69,91 @@ console.log(reverseArray([1, 2, 3, 4]));
 
 For example, if the input array is [5, 8, 2, 1, 10, 10, 8], the function should return [10, 10] since 10 is the largest number in the array, and it appears twice.*/
 
-const findMax = (arr) => {
-  let newArr = [];
-  let max = arr[0];
+// const findMax = (arr) => {
+//   let newArr = [];
+//   let max = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= max) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+
+//   return newArr;
+// };
+
+function findMax(numbers) {
+  var max = -Infinity;
+  var result = [];
+
+  // Find the maximum value in the array
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
     }
   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= max) {
-      newArr.push(arr[i]);
+  // Add all occurrences of the maximum value to the result array
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] === max) {
+      result.push(numbers[i]);
     }
   }
 
-  return newArr;
+  return result;
+}
+console.log(findMax([5, 8, 2, 1, 10, 10, 8, 70, 300, 100]));
+
+/*Write a function called isPrime that takes a positive integer as input and returns true if the number is prime, and false otherwise. A prime number is a number greater than 1 that is divisible by only 1 and itself.
+
+For example, the number 5 is a prime number because it is only divisible by 1 and 5. However, the number 4 is not prime because it is divisible by 1, 2, and 4.
+
+Your task is to implement the isPrime function and test it with different numbers to check if it correctly identifies prime numbers. */
+
+const isPrime = (num) => {
+  let isPrime = true;
+
+  if (num === 0 || num === 1) {
+    return (isPrime = false);
+  }
+  for (i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+    } else {
+      isPrime = true;
+    }
+  }
+  return isPrime;
+};
+console.log(isPrime(23));
+
+/*Write a JavaScript function called countVowels that takes a string as input and returns the count of vowels (a, e, i, o, u) present in the string. The function should ignore case, so both uppercase and lowercase vowels should be counted.
+
+For example, countVowels("Hello World") should return 3 because there are three vowels ('e', 'o', 'o') in the string "Hello World".
+
+Implement the countVowels function and test it with different strings to verify if it correctly counts the vowels.*/
+
+const countVowels = (str) => {
+  let newStr = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < newStr.length; i++) {
+    if (
+      newStr[i] == 'a' ||
+      newStr[i] == 'e' ||
+      newStr[i] == 'i' ||
+      newStr[i] == 'o' ||
+      newStr[i] == 'u'
+    ) {
+      count++;
+    }
+  }
+  return count;
 };
 
-console.log(findMax([5, 8, 2, 1, 10, 10, 8, 70, 300]));
+console.log(countVowels('i love watching anime'));
